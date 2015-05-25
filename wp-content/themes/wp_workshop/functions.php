0f0a6f501a42c4c5226f85f5a6c11129854c92bb
Dominic Vogl <dv@cat-ia.de>
2015-05-25 15:57:28 +0200

    [task] cleanup and add some comments

--- wp-content/themes/wp_workshop/functions.php
+++ wp-content/themes/wp_workshop/functions.php
@@ -1,18 +1,22 @@
 <?php
 
 
-// Definiere globale Konstanten um benötigte Inhalte zu speichern
-define('CURRENT_PAGE_ID', get_the_ID());
-define('TEMPLATE_VERSION', 1.0);
+// Definiere globale Konstanten um mehrfach benötigte Inhalte zu speichern
+
+define('CURRENT_PAGE_ID', get_the_ID()); // Die aktuelle ID eines Beitrags oder einer Seite, je nachdem was gerade aktiv ist
+define('TEMPLATE_VERSION', 1.0); // Version des Templates (wird für den Dateiimport benötigt)
 
 
-// List all file to import
+// Liste mit den Namen der einzubindenen Dateien
 $files = array(
    'template',
    'loops'
 );
 
+// Prüfen ob die Variable ein Array ist
 if (is_array($files)) {
+
+   // Binde die Dateien aus dem Array ein und lade deren inhalt
    foreach ($files as $filename) {
       include(TEMPLATEPATH . '/includes/' . $filename . '.php');
    }
