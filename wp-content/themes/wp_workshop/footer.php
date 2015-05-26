1e66ed3d471af101b0ef92ba7ac7701d74f3d0b4
Dominic Vogl <dv@cat-ia.de>
2015-05-26 21:12:07 +0200

    [task] rendere und style den footer

--- wp-content/themes/wp_workshop/footer.php
+++ wp-content/themes/wp_workshop/footer.php
@@ -1,3 +1,26 @@
+<div class="module module-footer">
+
+   <div class="row">
+      <div class="column small-12">
+         <div class="inner-content">
+            <?php
+
+            wp_nav_menu(
+               array(
+                  'container' => '',
+                  'menu_class' => 'footer-menu',
+                  'link_before' => '<span class="button">',
+                  'link_after' => '</span>'
+               )
+            );
+
+            ?>
+         </div>
+      </div>
+   </div>
+
+</div>
+
 <?php
 
 wp_footer();
