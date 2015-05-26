6c41ba09c1ad3ccf4bf8dadfcc1951dd5f465ec4
Dominic Vogl <dv@cat-ia.de>
2015-05-26 20:18:49 +0200

    [task] add slick slider scripts and some basic styles

---
+++ wp-content/themes/wp_workshop/js/custom.js
@@ -0,0 +1,14 @@
+//
+// custom.js
+// Autor: Dominic Vogl
+// ---------------------------------------------------------------
+
+// Wenn der DOM fertig geladen ist
+$(document).ready(function() {
+
+   $('.slick-slider').each(function() {
+      $(this).slick();
+   });
+
+});
+
