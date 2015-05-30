f410b5f95034a717c758c4d26bceedbb75c598fb
Dominic Vogl <dv@cat-ia.de>
2015-05-30 11:49:16 +0200

    [task] update slider functionality, autoplay is now configured

---
+++ wp-content/themes/wp_workshop/js/custom/custom.js
@@ -0,0 +1,31 @@
+//
+// custom.js
+// Autor: Dominic Vogl
+// ---------------------------------------------------------------
+
+
+
+// Wenn der DOM (Dokument) fertig geladen ist, dann tue etwas
+$(document).ready(function() {
+
+   // Initialisiere den Slide auf allen Elementen mit der angegebenen Klasse
+   slick_slider('.slick-slider');
+
+});
+
+/**
+ * Übergebe das Objekt an den Slider und initialisere diesen für jedes passende Element
+ * @param obj
+ */
+
+function slick_slider(obj) {
+
+   $(obj).each(function() {
+      $(this).slick({
+         autoplay: true,
+         speed: 600,
+         autoplaySpeed: 4000
+      });
+   });
+
+}
