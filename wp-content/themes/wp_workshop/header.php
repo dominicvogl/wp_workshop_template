63e026c24cb25591706386a9625123ddab132fd9
Dominic Vogl <dv@cat-ia.de>
2015-05-30 10:20:08 +0200

    [task] update header styles and rendering

--- wp-content/themes/wp_workshop/header.php
+++ wp-content/themes/wp_workshop/header.php
@@ -22,25 +22,23 @@
 <body <?php body_class(); ?>>
 
 <div class="module-header">
-   <div class="row">
-         <div class="inner-content">
-            <div class="row">
-               <div class="column small-12 medium-6 logo">
-                  <img src="<?php echo get_template_directory_uri().'/assets/logo.svg';?>" width="300" alt="Wordpress Logo"/>
-               </div>
+   <div class="inner-content">
+      <div class="row">
+            <div class="column small-12 medium-6 logo">
+               <img src="<?php echo get_template_directory_uri().'/assets/logo.svg';?>" width="300" alt="Wordpress Logo"/>
+            </div>
 
-               <div class="column small-12 medium-6">
-                  <?php
-                  wp_nav_menu(
-                     array(
-                        'container' => '',
-                        'menu_class' => 'header-menu',
-                        'link_before' => '<span class="button">',
-                        'link_after' => '</span>'
-                     )
-                  );
-                  ?>
-               </div>
+            <div class="column small-12 medium-6">
+               <?php
+               wp_nav_menu(
+                  array(
+                     'container' => '',
+                     'menu_class' => 'header-menu',
+                     'link_before' => '<span class="button">',
+                     'link_after' => '</span>'
+                  )
+               );
+               ?>
             </div>
          </div>
    </div>
