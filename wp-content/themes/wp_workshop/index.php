a563260ba30ca0cb31c1dc27dba04efa3b84a415
Dominic Vogl <dv@cat-ia.de>
2015-05-30 22:04:53 +0200

    [task] add styles for tag list in archive

--- wp-content/themes/wp_workshop/index.php
+++ wp-content/themes/wp_workshop/index.php
@@ -1,5 +1,7 @@
 <?php
 
+$post_ID = get_the_ID();
+
 get_header();
 
 if (have_posts()) {
@@ -7,24 +9,25 @@ if (have_posts()) {
    // Zeige die letzten Newsbeiträge an
    echo '<div class="row">';
 
-   if(is_archive()) {
+   if (is_archive()) {
       $cat_id = get_query_var('cat');
       $cat_obj = get_category($cat_id);
-      echo '<div class="column small-12"><h1>Archiv: '.$cat_obj->cat_name.'</h1></div>';
+      echo '<div class="column small-12"><h1>Archiv: ' . $cat_obj->cat_name . '</h1></div>';
    }
 
    while (have_posts()) {
       the_post();
       ?>
 
-      <div class="column small-12 medium-6 large-4">
+      <div class="column small-12 medium-6 large-4 post-single post-<?php echo $post->ID; ?>">
 
          <div class="inner-content">
 
             <h3><?php the_title(); ?></h3>
             <span class="post-date"><?php the_date(); ?></span>
+            <?php echo get_category_list($post_ID) ?>
             <?php the_content(''); ?>
-            <a href="<?php echo get_permalink(CURRENT_PAGE_ID); ?>" class="button">Zum Artikel</a>
+            <a href="<?php echo get_permalink($post_ID); ?>" class="button">Zum Artikel</a>
 
          </div>
 
